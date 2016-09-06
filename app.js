2d6e73cb54ab1df6ae7bf01e59f19272c391b0fb
Anton Filimonov <anton.filimonov@gmail.com>
2016-09-06 11:44:33 +0300

    Added v1 deprecation test support

--- app.js
+++ app.js
@@ -19,21 +19,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var authorization = require('./routes/authorization');
-var files = require('./routes/files');
 
-app.use('/1/oauth', authorization);
-app.use('/1/', files);
-app.use('/', function(req, res, next) {
-  res.sendStatus(200);
-});
+if (!process.env.DISABLE_V1)
+{
+  var authorization = require('./routes/authorization');
+  var files = require('./routes/files');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  app.use('/1/oauth', authorization);
+  app.use('/1/', files);
+  app.use('/', function(req, res, next) {
+    res.sendStatus(200);
+  });
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+}
+else {
+  app.use(function(req, res, next) {
+    res.status(400).json({error: "v1_retired"});
+  });
+}
+
 
 // error handlers
 app.use(function(err, req, res, next) {
